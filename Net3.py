2b3271bddeca6061cf4f908f508d95a2b0f64b9f
Celia-xy <yaooxii@gmail.com>
2016-05-10 10:29:41 -0400

    multi layer

---
+++ Net3.py
@@ -0,0 +1,135 @@
+
+from lasagne import layers
+from nolearn.lasagne import NeuralNet, BatchIterator
+import load
+import numpy as np
+from datetime import datetime
+from pandas import DataFrame
+from pandas.io.parsers import read_csv
+import os
+import theano
+
+# in each batch randomly flip half of the images
+
+
+class FlipBatchIterator2d(BatchIterator):
+    flip_indices = [
+        (0, 2), (1, 3),
+        (4, 8), (5, 9), (6, 10), (7, 11),
+        (12, 16), (13, 17), (14, 18), (15, 19),
+        (22, 24), (23, 25),
+        ]
+
+    def transform(self, Xb, yb):
+        Xb, yb = super(FlipBatchIterator2d, self).transform(Xb, yb)
+
+        # Flip half of the images in this batch at random:
+        bs = Xb.shape[0]
+        indices = np.random.choice(bs, bs / 2, replace=False)
+        Xb[indices] = Xb[indices, :, :, ::-1]
+
+        if yb is not None:
+            # Horizontal flip of all x coordinates:
+            yb[indices, ::2] = yb[indices, ::2] * -1
+
+            # Swap places, e.g. left_eye_center_x -> right_eye_center_x
+            for a, b in self.flip_indices:
+                yb[indices, a], yb[indices, b] = (
+                    yb[indices, b], yb[indices, a])
+
+        return Xb, yb
+
+# Changing learning rate and momentum over time
+
+
+def float32(k):
+    return np.cast['float32'](k)
+
+
+class AdjustVariable(object):
+    def __init__(self, name, start=0.03, stop=0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+
+        epoch = train_history[-1]['epoch']
+        new_value = float32(self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
+
+net3 = NeuralNet(
+    layers=[
+        ('input', layers.InputLayer),
+        ('conv1', layers.Conv2DLayer),
+        ('pool1', layers.MaxPool2DLayer),
+        ('conv2', layers.Conv2DLayer),
+        ('pool2', layers.MaxPool2DLayer),
+        ('conv3', layers.Conv2DLayer),
+        ('pool3', layers.MaxPool2DLayer),
+        ('hidden4', layers.DenseLayer),
+        ('hidden5', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+    input_shape=(None, 1, 96, 96),
+    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
+    hidden4_num_units=500, hidden5_num_units=500,
+    output_num_units=30, output_nonlinearity=None,
+
+    update_learning_rate=theano.shared(float32(0.03)),
+    update_momentum=theano.shared(float32(0.9)),
+
+    regression=True,
+    batch_iterator_train=FlipBatchIterator2d(batch_size=128),
+    on_epoch_finished=[
+        AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
+        AdjustVariable('update_momentum', start=0.9, stop=0.999),
+        ],
+    max_epochs=1000,
+    verbose=1,
+    )
+
+X, y = load.load2d()  # load 2-d data
+net3.fit(X, y)
+
+# Training for 1000 epochs will take a while.  We'll pickle the
+# trained model so that we can load it back later:
+import cPickle as pickle
+with open('net3.pickle', 'wb') as f:
+    pickle.dump(net3, f, -1)
+
+# make submission
+print 'start making submission'
+def submission(fname_net='net3.pickle'):
+    with open(fname_net, 'rb') as f:
+        net = pickle.load(f)     # net = specialists
+
+    X = load.load2d(test=True)[0]
+    y_pred = net.predict(X)
+    print 'Finish predict test file'
+    columns = 'left_eye_center_x,left_eye_center_y,right_eye_center_x,right_eye_center_y,left_eye_inner_corner_x,left_eye_inner_corner_y,left_eye_outer_corner_x,left_eye_outer_corner_y,right_eye_inner_corner_x,right_eye_inner_corner_y,right_eye_outer_corner_x,right_eye_outer_corner_y,left_eyebrow_inner_end_x,left_eyebrow_inner_end_y,left_eyebrow_outer_end_x,left_eyebrow_outer_end_y,right_eyebrow_inner_end_x,right_eyebrow_inner_end_y,right_eyebrow_outer_end_x,right_eyebrow_outer_end_y,nose_tip_x,nose_tip_y,mouth_left_corner_x,mouth_left_corner_y,mouth_right_corner_x,mouth_right_corner_y,mouth_center_top_lip_x,mouth_center_top_lip_y,mouth_center_bottom_lip_x,mouth_center_bottom_lip_y'
+    columns = columns.split(',')
+
+    y_pred = y_pred * 48 + 48
+    y_pred = y_pred.clip(0, 96)
+    df = DataFrame(y_pred, columns=columns)
+
+    lookup_table = read_csv(os.path.expanduser('./data/IdLookupTable.csv'))
+    values = []
+
+    for index, row in lookup_table.iterrows():
+        values.append((
+            row['RowId'],
+            df.ix[row.ImageId - 1][row.FeatureName],
+            ))
+
+    now_str = datetime.now().isoformat().replace(':', '-')
+    submission = DataFrame(values, columns=('RowId', 'Location'))
+    filename = 'submission-{}.csv'.format(now_str)
+    submission.to_csv(filename, index=False)
+    print("Wrote {}".format(filename))
+submission()
